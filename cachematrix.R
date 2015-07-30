eb80d0b794ce6ebbcd76e9e9147a5fcb6fb60050
eric <chunchet@gmail.com>
2015-07-30 12:48:52 +0800

    done

--- cachematrix.R
+++ cachematrix.R
@@ -6,7 +6,7 @@
 ## 1. set the value of the matrix
 ## 2. get the value of the matrix
 ## 3. set the value of the inverse matrix
-## 4. get the value of the inverse matrix
+## 4. get the value of the inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
 	inv <- NULL
